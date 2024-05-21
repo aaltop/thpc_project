89a0f8b3b3ce9c97ceaf63e7520f5a60487e9efe
TA <aaltopi@gmail.com>
2024-05-21 19:41:13 +0300

    Add passing process number to parallel run script from command line

--- run/parallel.sh
+++ run/parallel.sh
@@ -2,9 +2,7 @@
 
 input="parallel_args.txt"
 while IFS= read -r line; do
-        for i in {4,}; do
-            echo "args: $line"
-            # needs </dev/null because mpirun stops the while from running, apparently
-            mpirun --oversubscribe -n $i ./tsp_ga_parallel $line </dev/null
-        done
+        echo "args: $line"
+        # needs </dev/null because mpirun stops the while from running, apparently
+        mpirun --oversubscribe -n $1 ./tsp_ga_parallel $line </dev/null
 done < $input
